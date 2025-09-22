a47086197b63032ee3dc2c835b8ef9febdb1b327
erick <erick.sanabria@cosmic-chimps.com>
2025-09-22 12:14:45 -0600

    fix dokcer run

--- db/automate_backups.sh
+++ db/automate_backups.sh
@@ -14,12 +14,13 @@ today=$(date +%F)
 
 local_password=OcelOps
 local_user=ocelotl
-# Check if all arguments are provided
-if  [! which docker 2>/dev/null || echo FALSE]; then
+# Check if Docker is installed
+if ! command -v docker &> /dev/null; then
   echo "Docker is not properly installed"
   exit 1
 fi
 
+# Check if all arguments are provided
 if [ "$#" -ne 7 ]; then
     echo "Usage: $0 <engine> <host> <port> <user> <password> <database> <backup_dir>"
     exit 1
@@ -32,12 +33,15 @@ if [ docker images --filter=reference="$engine" | wc -l -eq 1 ]; then
     exit 1
 fi
 
+# Normalize Docker container name (replace : and . with -)
+container_name=$(echo "$engine" | sed 's/[:.]/-/g')
+
 if  [[ "$engine" == "mysql"* ]]; then
-  docker run --name $engine -p 3306:3306 -e MYSQL_ROOT_PASSWORD=securepassword -e MYSQL_DATABASE=mydatabase -e MYSQL_USER=$local_user -e MYSQL_PASSWORD=$local_password -v /data/mysql:/var/lib/mysql -d mysql:8.0
-  docker exec $engine /usr/bin/mysqldump -u $user --password=$password $database > $backup_dir/"$database_$today".sql
+  docker run --name $container_name -p 3306:3306 -e MYSQL_ROOT_PASSWORD=securepassword -e MYSQL_DATABASE=mydatabase -e MYSQL_USER=$local_user -e MYSQL_PASSWORD=$local_password -v /data/mysql:/var/lib/mysql -d $engine
+  docker exec $container_name /usr/bin/mysqldump -h $host -P $port -u $user --password=$password $database > $backup_dir/"$database_$today".sql
 fi
 
 if [[ "$engine" == "postgres"* ]]; then
-  docker run --name $engine -p 5432:5432 -e POSTGRES_PASSWORD=$password -e POSTGRES_USER=$user -e POSTGRES_DB=$database -v /data/postgres:/var/lib/postgresql/data -d postgres:15
-  docker exec $engine pg_dump -U $user $database > $backup_dir/"$database_$today".sql
+  docker run --name $container_name -p 5432:5432 -e POSTGRES_PASSWORD=$password -e POSTGRES_USER=$user -e POSTGRES_DB=$database -v /data/postgres:/var/lib/postgresql/data -d $engine
+  docker exec $container_name pg_dump -h $host -p $port -U $user $database > $backup_dir/"$database_$today".sql
 fi
